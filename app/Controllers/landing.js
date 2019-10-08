7adac2202d00d8f6b7dabae9aa107dbe91a999df
badgebotio <kerri@openworksgrp.com>
2019-10-08 12:42:18 -0400

    Adding twitter timeline to home page. Uses .env var for twitter username.

--- app/Controllers/landing.js
+++ app/Controllers/landing.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var twitterUsername = process.env.TWITTER_USERNAME;
+
 exports.read = function(req,res){
 
     return res.render('index', {
         title: "BadgeBot",
-        description: "The world's first Open Badges issuing Twitter bot."
+        description: "The world's first Open Badges issuing Twitter bot.",
+        twitterUsername: twitterUsername
     });
 };
